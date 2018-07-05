d08804e31aa458d004ea6a25feb43609b36d4b2b
Nathan Rajlich <nathan@tootallnate.net>
2018-07-04 17:13:33 -0700

    Refactor to use an LRU cache and cache responses

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const bytes = require('bytes');
 const {parse} = require('url');
+const LRU = require('lru-cache');
 const fetch = require('node-fetch');
+const toBuffer = require('raw-body');
 const {
   IMPORT_ORG = 'importpw',
   IMPORT_REPO = 'import'
@@ -9,25 +12,66 @@ const toURL = ({repo, org, ref, file}) => (
   `https://github.com/${org}/${repo}/raw/${ref}/${file}`
 );
 
+const cache = LRU({
+  max: bytes('10mb'),
+  length(n, key) {
+    return n.body.length;
+  },
+  dispose(key, n) {
+    console.log('dispose', { n, key });
+  }
+});
+
 module.exports = async (req, res) => {
+  let ref = 'master';
+  let org = IMPORT_ORG;
+  let repo = IMPORT_REPO;
   let {pathname, query: {file}} = parse(req.url, true);
+
+  const at = pathname.lastIndexOf('@');
+  if (at !== -1) {
+    ref = pathname.substring(at + 1);
+    pathname = pathname.substring(0, at);
+  }
   const parts = pathname.substring(1).split('/');
-  if (parts.length > 2) {
+  const numParts = parts.length;
+
+  if (numParts === 1) {
+    if (parts[0]) repo = parts[0];
+  } else if (numParts === 2) {
+    if (parts[0]) org = parts[0];
+    if (parts[1]) repo = parts[1];
+  } else {
     res.statusCode = 400;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`Too many slashes in the URL\n`);
-    return;
+    return `Expected up to 2 slashes in the URL, but got ${numParts}\n`;
   }
-  let repo = parts.pop() || IMPORT_REPO;
-  const org = parts.pop() || IMPORT_ORG;
-  let ref = 'master';
-  const at = repo.indexOf('@');
-  if (at !== -1) {
-    ref = repo.substring(at + 1);
-    repo = repo.substring(0, at);
+  if (!file) file = `${repo}.sh`;
+  const params = {repo, org, ref, file};
+  const id = JSON.stringify(params);
+  let cached = cache.get(id);
+
+  if (!cached) {
+    const url = toURL(params);
+    const res2 = await fetch(url);
+    const body = await toBuffer(res2.body, {
+      limit: '1mb'
+    });
+    cached = {
+      body,
+      status: res2.status,
+      headers: {
+        'Content-Type': res2.headers.get('Content-Type'),
+        'ETag': res2.headers.get('ETag'),
+        'Content-Location': res2.url
+      }
+    };
+    cache.set(id, cached);
   }
-  const url = toURL({repo, org, ref, file: file || `${repo}.sh`});
-  res.statusCode = 302;
-  res.setHeader('Location', url);
-  res.end(`Redirecting to ${JSON.stringify(url)}\n`);
+
+  res.statusCode = cached.status;
+  for (const name of Object.keys(cached.headers)) {
+    res.setHeader(name, cached.headers[name]);
+  }
+  return cached.body;
 };
