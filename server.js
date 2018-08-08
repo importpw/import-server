b30d071a85c987a0d4af64d1020f641b50b0eba6
Nathan Rajlich <nathan@tootallnate.net>
2018-08-07 21:34:01 -0700

    Add a root HTML file and default styling

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const bytes = require('bytes');
 const {parse} = require('url');
 const LRU = require('lru-cache');
+const {resolve} = require('path');
 const fetch = require('node-fetch');
 const toBuffer = require('raw-body');
+const {readFileSync} = require('fs');
 const {createElement} = require('react');
 const {default: MDX} = require('@mdx-js/runtime');
-const {renderToStaticMarkup, renderToStaticNodeStream} = require('react-dom/server');
+const {renderToStaticNodeStream} = require('react-dom/server');
 const {IMPORT_ORG = 'importpw', IMPORT_REPO = 'import'} = process.env;
+
+const indexTemplate = resolve(__dirname, 'templates', 'index.html');
+const [HTML_START, HTML_END] = readFileSync(indexTemplate, 'utf8').split('__CONTENT__');
 
 const toURL = ({repo, org, ref, file}) => (
   `https://raw.githubusercontent.com/${org}/${repo}/${ref}/${file}`
@@ -62,11 +67,16 @@ module.exports = async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     return `Expected up to 2 slashes in the URL, but got ${numParts}\n`;
   }
+
   if (isHTML) {
     // TODO: Support case-insensitive filename, and non-markdown
     file = 'Readme.md';
   }
-  if (!file) file = `${repo}.sh`;
+
+  if (!file) {
+    file = `${repo}.sh`;
+  }
+
   const params = {repo, org, ref, file};
   const id = JSON.stringify(params);
   let cached = cache.get(id);
@@ -96,11 +106,17 @@ module.exports = async (req, res) => {
   }
 
   if (isHTML) {
+    // Render the readme as MDX
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
     const el = createElement(MDX, {
       children: cached.body.toString('utf8')
     });
-    renderToStaticNodeStream(el).pipe(res);
+    const reactStream = renderToStaticNodeStream(el);
+    res.write(HTML_START);
+    reactStream.pipe(res, {end: false});
+    reactStream.on('end', () => {
+      res.end(HTML_END);
+    });
   } else {
     res.statusCode = cached.status;
     for (const name of Object.keys(cached.headers)) {
