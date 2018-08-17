71bac28e3245228566ad4767356b94738c03ae1c
Evil Rabbit <hello@evilrabb.it>
2018-08-17 01:28:23 -0300

    Add styles to hr

--- pages/layout.js
+++ pages/layout.js
@@ -185,6 +185,13 @@ export default class extends React.Component {
             padding: 0;
           }
 
+          hr {
+              border: 0;
+              height: 0;
+              border-top: 1px solid rgba(0, 0, 0, 0.1);
+              border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+          }
+
           img {
             max-width: 100%;
           }
