9f154fd7d2e415439fb7ea5cd3405d6e1b9afd8d
Nathan Rajlich <nathan@tootallnate.net>
2018-08-23 10:26:38 -0700

    Use global styles for Highlight.js
    Easier to modify them this way.

--- components/code.js
+++ components/code.js
@@ -1,9 +1,8 @@
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { grayscale } from 'react-syntax-highlighter/styles/hljs';
 
 export default function MarkdownCode (props) {
   if (props.language) {
-    return <SyntaxHighlighter language={props.language} style={grayscale}>{props.value}</SyntaxHighlighter>;
+    return <SyntaxHighlighter language={props.language} useInlineStyles={false}>{props.value}</SyntaxHighlighter>;
   } else {
     return <pre><code className="hljs">{props.value}</code></pre>;
   }
