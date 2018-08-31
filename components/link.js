d90e1c4d882184a64e7f360f4dc4b6a846f68060
Nathan Rajlich <nathan@tootallnate.net>
2018-08-31 12:19:15 -0700

    Fix relative links
    By introducing a `Curry` component, we can pass in the parent
    props to the `MarkdownLink` component so that it has the necessary
    context to know about which org / repo is being rendered. And
    with that information we can fix relative links to match the URL
    structure of `import.pw-server`.

--- components/link.js
+++ components/link.js
@@ -1,8 +1,15 @@
+import { join } from 'path';
 import Link from 'next/link';
 
 export default function MarkdownLink (props) {
   const ownProps = Object.assign({}, props);
   let { href } = ownProps;
   delete ownProps.href;
-  return <Link href={href}><a {...ownProps}>{props.children}</a></Link>;
+
+  const isRelative = /^\.\.?\//.test(href);
+  if (isRelative) {
+    href = '/' + join(props.org, props.repo, href);
+  }
+
+  return <Link href={href}><a>{props.children}</a></Link>;
 }
