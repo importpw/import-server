e71ecc796a6ee9a27afb61174113f89129fe3321
Nathan Rajlich <nathan@tootallnate.net>
2020-05-24 00:48:26 -0700

    Update to Vercel

---
+++ components/icons/vercel.js
@@ -0,0 +1,14 @@
+export default props => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 116 100"
+		fill="#000"
+		{...props}
+	>
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M57.5 0L115 100H0L57.5 0z"
+		/>
+	</svg>
+);
