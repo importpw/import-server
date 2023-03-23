5200e5c5d21dd621933f1e00607dbed3adb0f4f0
Nathan Rajlich <n@n8.io>
2023-03-23 00:06:49 -0700

    Fix

--- pages/[...rest].js
+++ pages/[...rest].js
@@ -250,7 +250,7 @@ export default class Page extends React.Component {
 					<meta property="og:title" content={title} />
 					<meta property="og:description" content={repoDescription} />
 					<meta property="og:type" content="website" />
-					<meta property="description" content={repoDescription} />
+					<meta name="description" content={repoDescription} />
 				</Head>
 
 				<div className="header">
